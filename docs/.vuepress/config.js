5ed3383331d357a6ca94cdc252016e23a2122e02
WidgetKing <tristan.ward@infosemantics.com.au>
2020-03-20 14:55:24 +0800

    Local dev now working

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,12 @@
 module.exports = {
-	title: "Hello VuePress",
-	description:"Just playing around",
-	base: "doc-test-vuepress"
-}
+  title: 'This is a working Vuepress site',
+  description: 'This is the description alteration',
+  // base: 'doc-test-vuepress',
+  themeConfig: {
+    nav: [
+		{text: 'Home', link: '/'},
+		{text: 'Guide', link: '/guide/'},
+		{text: 'About Me', link: '/About'}
+	],
+  },
+};
