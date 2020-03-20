8af649bbc15d0dc1d9b92629fb4bbcd81e2f3929
WidgetKing <tristan.ward@infosemantics.com.au>
2020-03-20 15:28:25 +0800

    Another vuepress fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,12 @@
 module.exports = {
   title: 'This is a working Vuepress site',
   description: 'This is the description alteration',
+  base: '/doc-test-vuepress/',
   themeConfig: {
     nav: [
-		{text: 'Home', link: '/'},
-		{text: 'Guide', link: '/guide/'},
-		{text: 'About Me', link: '/About'}
-	],
+      {text: 'Home', link: '/'},
+      {text: 'Guide', link: '/guide/'},
+      {text: 'About Me', link: '/About'},
+    ],
   },
 };
